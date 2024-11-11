63d723b6352f9d9353dd15da1c739c8b3522c889
Haggai Khafizov <hafyok5777@gmail.com>
2024-11-11 17:54:00 +0300

    Add dial time with time picker

---
+++ app/src/main/java/com/example/workmanager/presentation/DialTime.kt
@@ -0,0 +1,63 @@
+package com.example.workmanager.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberTimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import java.util.Calendar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DialTime(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    setShowDialog: (Boolean) -> Unit
+) {
+    val currentTime = Calendar.getInstance()
+
+    val timePickerState = rememberTimePickerState(
+        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
+        initialMinute = currentTime.get(Calendar.MINUTE),
+        is24Hour = true,
+    )
+
+    Dialog(onDismissRequest = { setShowDialog(false) }) {
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = Color.Gray
+        ) {
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth().padding(4.dp)) {
+                Column(verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+                    TimePicker(
+                        state = timePickerState,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()){
+                        Button(onClick = onDismiss) {
+                            Text(text = "Dismiss picker")
+                        }
+                        Button(onClick = onConfirm) {
+                            Text(text = "Confirm selection")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
