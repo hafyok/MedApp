ca720e252e675bf0cfa02425abe3cb2f9149e7ba
Haggai Khafizov <hafyok5777@gmail.com>
2024-11-21 16:32:14 +0300

    Pushes work from console firebase

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.workmanager
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.workmanager.notification.PushService
 import com.example.workmanager.presentation.MainScreen
 import com.example.workmanager.ui.theme.WorkManagerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,9 +24,40 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private lateinit var pushBroadcastReceiver: BroadcastReceiver
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        pushBroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val extras = intent?.extras
+                Log.e("TAG", "Message received")
+                extras?.keySet()?.firstOrNull { it == PushService.KEY_ACTION }?.let { key ->
+                    Log.e("TAG", "Action key -> $key")
+                    when (extras.getString(key)) {
+                        PushService.ACTION_SHOW_MESSAGE -> {
+                            extras.getString(PushService.KEY_MESSAGE)?.let { message ->
+                                Log.e("TAG", "Message key -> $message")
+                                Toast.makeText(
+                                    applicationContext,
+                                    message,
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                        }
+                        else -> Log.e("TAG", "No needed key found")
+                    }
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(PushService.INTENT_FILTER)
+        registerReceiver(pushBroadcastReceiver, intentFilter, RECEIVER_NOT_EXPORTED)
+
         enableEdgeToEdge()
         setContent {
             WorkManagerTheme {
@@ -26,6 +66,11 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(pushBroadcastReceiver)
+        super.onDestroy()
     }
 
 }
--- app/src/main/java/com/example/workmanager/notification/PushService.kt
+++ app/src/main/java/com/example/workmanager/notification/PushService.kt
@@ -1,11 +1,33 @@
 package com.example.workmanager.notification
 
+import android.content.Intent
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class PushService : FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
+
+        // Send registration token to server
     }
 
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        val intent = Intent(INTENT_FILTER)
+        message.data.forEach{ entity ->
+            intent.putExtra(entity.key, entity.value)
+        }
+
+        sendBroadcast(intent)
+    }
+
+    companion object{
+        const val INTENT_FILTER = "PUSH_EVENT"
+        const val KEY_ACTION = "action"
+        const val KEY_MESSAGE = "message"
+
+        const val ACTION_SHOW_MESSAGE = "show_message"
+    }
 
 }
