502a488ea3ceb553be1dcd1686e92233fd109fb7
Haggai Khafizov <hafyok5777@gmail.com>
2024-12-02 16:03:53 +0300

    Add DoseScheduleEntity

--- app/src/main/java/com/example/workmanager/data/AppDB.kt
+++ app/src/main/java/com/example/workmanager/data/AppDB.kt
@@ -3,7 +3,7 @@ package com.example.workmanager.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(version = 1, entities = [MedicamentEntity::class], )
+@Database(version = 2, entities = [MedicamentEntity::class, DoseScheduleEntity::class], )
 abstract class AppDB: RoomDatabase() {
     abstract val dao: MedicamentDao
 }
