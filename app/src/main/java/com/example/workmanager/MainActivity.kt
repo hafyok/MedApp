9318a615868ff4970580b6473732abf2fdffa3a8
Haggai Khafizov <hafyok5777@gmail.com>
2024-12-09 21:33:40 +0300

    Test notification

--- app/src/main/java/com/example/workmanager/MainActivity.kt
+++ app/src/main/java/com/example/workmanager/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.workmanager
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,16 +10,50 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.work.Constraints
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.workmanager.presentation.MainScreen
 import com.example.workmanager.ui.theme.WorkManagerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val constraints = Constraints.Builder()
+        .setRequiresCharging(true)
+        .build()
+
+    val workRequest = OneTimeWorkRequestBuilder<DemoWorker>()
+        .setConstraints(constraints)
+        .addTag("myTask")
+        .build()
+
+    val otherWorkRequest = OneTimeWorkRequestBuilder<OtherWorker>()
+        .addTag("otherTask")
+        .build()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
+
+        WorkManager.getInstance(this)
+            .enqueueUniqueWork(
+                "DemoWorker",
+                ExistingWorkPolicy.REPLACE,
+                workRequest
+            )
+        /*WorkManager.getInstance(this)
+            .beginWith(workRequest)
+            .then(otherWorkRequest)
+            .enqueue()*/
         enableEdgeToEdge()
         setContent {
             WorkManagerTheme {
--- app/src/main/java/com/example/workmanager/MyWorker.kt
+++ app/src/main/java/com/example/workmanager/MyWorker.kt
@@ -1,12 +1,24 @@
 package com.example.workmanager
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.work.CoroutineWorker
-import androidx.work.Worker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import kotlinx.coroutines.delay
+import kotlin.properties.Delegates
 
 class MyWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
@@ -16,14 +28,120 @@ class MyWorker(appContext: Context, workerParams: WorkerParameters) :
         const val Progress = "Progress"
         private const val delayDuration = 1L
     }
+    private val notificationChannelId = "DemoNotificationChannelId"
 
+    fun notification(): Notification {
+        val intent = WorkManager.getInstance(applicationContext).createCancelPendingIntent(id)
+
+        return NotificationCompat.Builder(applicationContext, "test").setContentTitle("Test title")
+            .setContentText("Test text")
+            .setSmallIcon(android.R.drawable.ic_delete)
+            .addAction(
+                android.R.drawable.ic_delete,
+                "cancel",
+                intent
+            )
+            .build()
+    }
+
+    suspend fun testWorker() {
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotification(): Notification{
+        createNotificationChannel()
+
+        /*val notification = notification()
+        return ForegroundInfo(7, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)*/
+        createNotificationChannel()
+
+        val mainActivityIntent = Intent(
+            applicationContext,
+            MainActivity::class.java)
+
+        var pendingIntentFlag by Delegates.notNull<Int>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            pendingIntentFlag = PendingIntent.FLAG_IMMUTABLE
+        } else {
+            pendingIntentFlag = PendingIntent.FLAG_UPDATE_CURRENT
+        }
+
+        val mainActivityPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            mainActivityIntent,
+            pendingIntentFlag)
+
+
+        return NotificationCompat.Builder(
+            applicationContext,
+            notificationChannelId
+        )
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(applicationContext.getString(R.string.app_name))
+            .setContentText("Work Request Done!")
+            .setContentIntent(mainActivityPendingIntent)
+            .setAutoCancel(true)
+            .build()
+    }
+
+    /*@RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "test",
+                "WorkManager Notifications",
+                NotificationManager.IMPORTANCE_HIGH // Установите HIGH для теста
+            )
+            val notificationManager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (notificationManager.getNotificationChannel("test") == null) {
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+    }*/
+
+    private fun createNotificationChannel()
+    {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                notificationChannelId,
+                "DemoWorker",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+
+            val notificationManager: NotificationManager? =
+                getSystemService(
+                    applicationContext,
+                    NotificationManager::class.java)
+
+            notificationManager?.createNotificationChannel(
+                notificationChannel
+            )
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        return ForegroundInfo(
+            0, createNotification()
+        )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun doWork(): Result {
+        //setForeground(createForegroundInfo())
         Log.d(TAG, "doWork: start")
+
+
         val firstUpdate = workDataOf(Progress to 0)
         val lastUpdate = workDataOf(Progress to 100)
         setProgress(firstUpdate)
         delay(delayDuration)
         setProgress(lastUpdate)
+
 
         Log.d(TAG, "doWork: end")
         return Result.success()
